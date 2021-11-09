2560bb08ebc3ccb4132b6a588db39b50f7a2d6a8
benlansdell <ben.lansdell@stjude.org>
2021-11-09 15:51:18 -0600

    Update reqs

--- behaveml/features.py
+++ behaveml/features.py
@@ -2,6 +2,4 @@
 import pandas as pd
 
 from behaveml.dl.dl_features import compute_dl_probability_features
-
-def create_mars_features(df : pd.DataFrame, raw_col_names : list, animal_setup : dict):
-    return pd.DataFrame(df) 
+from behaveml.mars_features import compute_mars_features

4a5961a5e25de0774893a9702f42d812af8d99b8
benlansdell <ben.lansdell@stjude.org>
2022-01-31 21:36:32 -0600

    Add a separate config file that houses some parameters

---
+++ behaveml/config.py
@@ -0,0 +1,12 @@
+"""Configuration options for behaveml functions."""
+
+#TODO
+#Add support for the user to change these.
+
+global_config = {
+    'make_movie__y_offset': 60,
+    'make_movie__y_inc': 30,
+    'make_movie__text_color': 'green',
+    'make_movie__font_size': 36,
+    'create_mosaic_video__max_mosaic_vids': 16
+}

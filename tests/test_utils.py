54dd188239098b3419971a1f4761abb698a41181
benlansdell <ben.lansdell@stjude.org>
2022-03-26 11:25:44 -0500

    Move site to root

--- tests/test_utils.py
+++
@@ -1,9 +0,0 @@
-#####################
-## Setup test code ##
-#####################
-
-import pytest
-
-def test_ffmpeg():
-    from behaveml.utils import checkFFMPEG
-    assert type(checkFFMPEG()) is bool
